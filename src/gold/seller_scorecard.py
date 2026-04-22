340c2164ecf648d864f1f2fff1c1f4941f6101f8
luizhtc <luizhtrincac@gmail.com>
2026-04-22 17:44:38 +0000

    feat: delivery_performance table

--- src/gold/seller_scorecard.py
+++ src/gold/seller_scorecard.py
@@ -1,6 +1,24 @@
 # Databricks notebook source
-df = spark.read.table("cat_olist.sch_bronze.tbl_olist_order_reviews_dataset")
+from utils import read_from_silver
+from pyspark.sql.functions import col, countDistinct, sum as _sum
 
 # COMMAND ----------
 
-df.display()
+orders = read_from_silver("order")
+order_items = read_from_silver("order_items")
+reviews = read_from_silver("reviews")
+
+# COMMAND ----------
+
+order_items.groupby("seller_id","seller_state").agg(
+    countDistinct(col("order_id")).alias("total_orders"),
+    _sum(col("price")).alias("total_revenue")
+).display()
+
+# COMMAND ----------
+
+reviews.display()
+
+# COMMAND ----------
+
+
